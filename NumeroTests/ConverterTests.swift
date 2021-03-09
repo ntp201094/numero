36b654956de8980eeba3c8a14db4c5a56b8a1640
Phuc Nguyen <ntp201094@gmail.com>
2021-03-09 18:31:41 +0700

    First build

---
+++ NumeroTests/ConverterTests.swift
@@ -0,0 +1,29 @@
+//
+//  ConverterTests.swift
+//  NumeroTests
+//
+//  Created by Phuc Nguyen on 09/03/2021.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+    
+    let converter = Converter()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testConversionForOne() {
+        let result = self.converter.convert(1)
+        XCTAssertEqual(result, "", "Conversion for 1 is incorrect")
+    }
+
+}
